9cac2883806c363545e5ccb238b243651badee69
Brimah Khalil Kamara <brimahk@gmail.com>
2025-12-14 22:40:10 +0000

    Modified

--- 06-route.tf
+++ 06-route.tf
@@ -1,82 +1,51 @@
 # -----------------------
-# Route
-# -----------------------
-
-
-
-# -----------------------
-# Route Public
-# -----------------------
-
 # Public Route Table
+# -----------------------
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.architecture_vpc.id
 
   tags = {
-    Name = "${var.env_prefix}-PublicRouteTable"
+    Name = "${var.env_prefix}-Public-Web-rt"
   }
 }
 
-# Default route to the existing Internet Gateway
 resource "aws_route" "public_default_route" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.myapp-igw.id
+  gateway_id             = aws_internet_gateway.myapp_igw.id
 }
 
-# Associate both Public-Web subnets with the PublicRouteTable
 resource "aws_route_table_association" "public_assoc" {
-  for_each = {
-    az1 = aws_subnet.tiered["Public-Web-AZ-1"].id
-    az2 = aws_subnet.tiered["Public-Web-AZ-2"].id
-  }
+  for_each = var.public_subnets
 
-  subnet_id      = each.value
+  subnet_id      = aws_subnet.public[each.key].id
   route_table_id = aws_route_table.public.id
 }
 
-
 # -----------------------
-# Route Private
+# Private Route Tables
 # -----------------------
 
-
-# Private App Route Tables
 resource "aws_route_table" "private_app" {
-  for_each = {
-    az1 = {
-      subnet_id = aws_subnet.tiered["Private-App-AZ-1"].id
-      nat_id    = aws_nat_gateway.nat["az1"].id
-      name      = "Private-App-RT-AZ1"
-    }
-    az2 = {
-      subnet_id = aws_subnet.tiered["Private-App-AZ-2"].id
-      nat_id    = aws_nat_gateway.nat["az2"].id
-      name      = "Private-App-RT-AZ2"
-    }
-  }
+  for_each = var.private_subnets
 
   vpc_id = aws_vpc.architecture_vpc.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = each.value.nat_id
+    # NAT gateway selected purely by AZ
+    nat_gateway_id = aws_nat_gateway.nat[each.value.az].id
   }
 
   tags = {
-    Name = each.value.name
+    Name = "${var.env_prefix}-Private-${each.key}-rt"
   }
 }
 
 resource "aws_route_table_association" "private_app_assoc" {
-  for_each = {
-    for k, v in aws_route_table.private_app :
-    k => {
-      subnet_id      = aws_subnet.tiered["Private-App-AZ-${substr(k, 2, 1)}"].id
-      route_table_id = v.id
-    }
-  }
+  for_each = var.private_subnets
 
-  subnet_id      = each.value.subnet_id
-  route_table_id = each.value.route_table_id
+  subnet_id      = aws_subnet.private[each.key].id
+  route_table_id = aws_route_table.private_app[each.key].id
 }
