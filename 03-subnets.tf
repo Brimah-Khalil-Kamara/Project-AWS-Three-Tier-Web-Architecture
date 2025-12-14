87f60b26fb41ac7c46a64714a588b9aad9b9cb30
Brimah Khalil Kamara <brimahk@gmail.com>
2025-12-14 22:38:13 +0000

    Modified

--- 03-subnets.tf
+++ 03-subnets.tf
@@ -1,27 +1,25 @@
-//SUBNET RESOURCE
+resource "aws_subnet" "public" {
+  for_each = var.public_subnets
 
-
-/*
-resource "aws_subnet" "Public-Web-Subnet-AZ-1"{                
-    vpc_id = aws_vpc.architecture_vpc.id                      
-    cidr_block = var.subnet_cidr_block                
-    availability_zone = var.availability_zones                 
-    tags = {                                           
-    Name: "${var.env_prefix}-subnet-1"                
-    }
-}
-*/
-
-resource "aws_subnet" "tiered" {
-  for_each = { for idx, subnet in local.all_subnets : "${subnet.name}-AZ-${subnet.az_index + 1}" => subnet }
-
-  vpc_id                  = aws_vpc.architecture_vpc.id
-  availability_zone       = var.availability_zones[each.value.az_index]
-  cidr_block              = each.value.cidr
-  map_public_ip_on_launch = each.value.map_public
+  vpc_id            = aws_vpc.architecture_vpc.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.az
 
   tags = {
-    Name = "${var.env_prefix}-${each.value.name}-Subnet-AZ-${each.value.az_index + 1}"
+    Name        = "Public-${each.key}"
+    Environment = var.env_prefix
+  }
+}
 
+resource "aws_subnet" "private" {
+  for_each = var.private_subnets
+
+  vpc_id            = aws_vpc.architecture_vpc.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.az
+
+  tags = {
+    Name        = "Private-${each.key}"
+    Environment = var.env_prefix
   }
 }
