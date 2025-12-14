9f5cb58e46b5c2b50ab460221b593ec889b9ce40
Brimah Khalil Kamara <brimahk@gmail.com>
2025-12-14 22:39:02 +0000

    Modified

--- 04-igw.tf
+++ 04-igw.tf
@@ -1,6 +1,6 @@
 //INTERNET GATEWAY RESOURCE
 
-resource "aws_internet_gateway" "myapp-igw" {                       
+resource "aws_internet_gateway" "myapp_igw" {                       
     vpc_id = aws_vpc.architecture_vpc.id                                        
     tags = {                                                                                                          
       Name: "${var.env_prefix}-igw"                                  
